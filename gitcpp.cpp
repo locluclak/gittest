bf04d3bdd6484387a0e72435f15f88fc91ad97b3
locluklak <dtanloc.j@gmail.com>
2023-12-25 16:32:52 +0700

    first commit

---
+++ gitcpp.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "hello world" <<endl;
+    cout << "abc";
+
+      cout << "hello world" <<endl;
+    cout << "abc";
+      cout << "hello world" <<endl;
+    cout << "abc";
+
+
+      cout << "hello world" <<endl;
+    cout << "abc";
+
+    
+}
